9ac4476a4df5d54a996f95f2813e372dd4ef41bb
Giel Willemsen <33036522+GWillemsen@users.noreply.github.com>
2019-12-17 21:25:15 +0100

    Created the simulator with multi threading (#1)
    * Working rule set.
    * Working proper (fully optimized) multi threading simulator working

--- src/world.cpp
+++ src/world.cpp
@@ -1,3 +1,8 @@
+#include <thread>
+#include <mutex>
+#include <iterator>
+#include <chrono>
+
 #include "world.h"
 #include "cell.h"
 
@@ -13,6 +18,135 @@ void World::EmptyWorld()
 }
 
 // Public methods
+
+World::World()
+{
+	this->totalThreads = 0;
+	this->cancelSimulation = false;
+	this->pauzeSimulation = true;
+	this->currentGeneration = 0;
+	this->lastPartGeneration = 0;
+	this->lastUpdateDuration = 0;
+	this->targetSimulationSpeed = 1.0f;
+	
+	this->InitializeThreads();
+}
+
+void World::InitializeThreads()
+{
+	using std::pair;
+	using std::make_pair;
+	// Returns 0 when not able to detect, otherwise, the number of (logical) processors
+	this->totalThreads = std::thread::hardware_concurrency();
+	// Failsafe if not working properly
+	if (this->totalThreads == 0)
+		this->totalThreads = 1;
+
+	// If we only get 2 threads or less, use 1 thread for the simulator.
+	// Otherwise 1 for rendering, 1 for system and the others for the simulator.
+	if (this->totalThreads < 3)
+		this->totalThreads = 1;
+	else
+		this->totalThreads -= 2;
+	
+	// Calculate the number of thread that are extra (those will be created in the for loop below).
+	this->totalThreads--;
+	for (unsigned int m_threadId = 0; m_threadId < this->totalThreads; m_threadId++)
+	{
+		this->simUpdaters.emplace_back(std::thread(&World::ProcessPartContinuesly, this, m_threadId, this->totalThreads));
+		this->threadComboData.emplace(make_pair(m_threadId, new ThreadCombo()));
+	}
+	// Set the total thread count to the number that we will actually be using.
+	this->totalThreads++;
+	this->lastPartProcessor = std::thread(&World::ProcessLastPart, this);
+
+	// Start the timer
+	this->timerThread = std::thread(&World::TimerThread, this);
+}
+
+// 1. copy constructor
+World::World(const World& that) : author(that.author), filePath(that.filePath), description(that.description) 
+{
+	this->cancelSimulation = that.cancelSimulation;
+	this->lastUpdateDuration = 0;
+
+	this->pauzeSimulation = that.pauzeSimulation;
+	InitializeThreads();
+	for (auto m_cell : that.cells)
+	{
+		this->cells.insert(
+			std::make_pair(
+				std::make_pair(m_cell.first.first, m_cell.first.second),
+				new Cell(m_cell.second->x, m_cell.second->y, m_cell.second->cellState)
+			)
+		);
+	}
+	this->currentGeneration = that.currentGeneration;
+	this->targetSimulationSpeed = that.targetSimulationSpeed;
+	this->lastPartGeneration = that.lastPartGeneration;
+}
+
+// 2. copy assignment operator
+World& World::operator=(const World& that) 
+{
+	this->cancelSimulation = that.cancelSimulation;
+	this->lastUpdateDuration = 0;
+	
+	this->author = that.author;
+	this->filePath = that.filePath;
+	this->description = that.description;
+
+	this->pauzeSimulation = that.pauzeSimulation;
+	InitializeThreads();
+	for (auto m_cell : that.cells)
+	{
+		this->cells.insert(
+			std::make_pair(
+				std::make_pair(m_cell.first.first, m_cell.first.second),
+				new Cell(m_cell.second->x, m_cell.second->y, m_cell.second->cellState)
+			)
+		);
+	}
+	this->currentGeneration = that.currentGeneration;
+	this->targetSimulationSpeed = that.targetSimulationSpeed;
+	this->lastPartGeneration = that.lastPartGeneration;
+	return *this;
+}
+
+// 3. destructor
+World::~World()
+{
+	this->PauzeSimulation();
+	// Start canceling the simulator updater's
+	{
+		std::lock_guard<std::mutex> m_lk(this->simCalcUpdateLock);
+		this->cancelSimulation = true;
+	}
+	this->simCalcUpdate.notify_all();
+	this->nextUpdateCv.notify_all();
+
+	// Wait on all threads to return
+	auto m_begining = this->simUpdaters.begin();
+	auto m_end = this->simUpdaters.end();
+	while (m_begining != m_end)
+	{
+		if (m_begining->joinable())
+			m_begining->join();
+		
+		std::advance(m_begining, 1);
+	}
+	this->simUpdaters.clear();
+	this->timerThread.join();
+	this->lastPartProcessor.join();
+
+	// Remove all cell data
+	for (auto m_cell : this->cells)
+	{
+		delete m_cell.second;
+	}
+	this->cells.clear();
+}
+
 void World::Save(std::string a_worldName)
 {
 	// Saves the world to a file
@@ -30,12 +164,20 @@ void World::SaveAsTemplate(std::string a_worldName)
 
 void World::StartSimulation()
 {
-	// Starts the simulation
+	{
+		std::lock_guard<std::mutex> m_lk(this->simCalcUpdateLock);
+		this->pauzeSimulation = false;
+	}
+	this->lastPartGenerationCv.notify_all();
 }
 
 void World::PauzeSimulation()
 {
-	// Pauzes the simulation
+	{
+		std::lock_guard<std::mutex> m_lk(this->simCalcUpdateLock);
+		this->pauzeSimulation = true;
+	}
+	this->lastPartGenerationCv.notify_all();
 }
 
 void World::ResetSimulation()
@@ -43,17 +185,357 @@ void World::ResetSimulation()
 	// Resets the simulation
 }
 
-void World::UpdateSimulation()
+void World::UpdateSimulationWithSingleGeneration()
 {
-	// Updates the simulation
+	{
+		std::lock_guard<std::mutex> m_lk(this->currentGenerationLock);
+		this->currentGeneration++;
+	}
+	this->nextUpdateCv.notify_all();
 
-	// Write simulation logic here...
+
+	auto m_begining = this->threadComboData.begin();
+	auto m_ending = this->threadComboData.end();
+	
+	// Wait for the processing threads to catch up to the current generation
+	while (m_begining != m_ending)
+	{
+		ThreadCombo* m_threadData = m_begining->second;
+		unsigned long m_lastGenerationValue = m_threadData->current_generation;
+		if (m_lastGenerationValue != this->currentGeneration)
+		{
+			std::unique_lock<std::mutex> m_locker(m_threadData->lock);
+			m_begining->second->cv.wait(m_locker, [this, m_threadData] {
+				return m_threadData->current_generation >= this->currentGeneration;
+			});
+		}
+		std::advance(m_begining, 1);
+	}
+	
+	// Check that the last part thread also is finished
+	// Only go into wait if the thread hasn't already caught up
+	bool m_wait = false;
+	{
+		std::lock_guard<std::mutex> m_locker(this->lastPartLock);
+		if (this->lastPartGeneration != this->currentGeneration)
+			m_wait = true;
+	}
+
+	if (m_wait)
+	{
+		std::unique_lock<std::mutex> m_locker(this->lastPartLock);
+		this->lastPartGenerationCv.wait(m_locker, [this] {
+			return this->lastPartGeneration >= this->currentGeneration;
+		});
+		m_locker.unlock();
+	}
+
+	//TODO multi thread this too?
+	// Process the calculated results
+	for (auto m_cellPair : this->cells)
+	{
+		if ((m_cellPair.second->atomic_neighborCount.load() == 1 ||
+			 m_cellPair.second->atomic_neighborCount.load() == 2) &&
+			m_cellPair.second->cellState != Background &&
+			m_cellPair.second->cellState != Tail)
+		{
+			m_cellPair.second->decayState = Head;
+		}
+		m_cellPair.second->cellState = m_cellPair.second->decayState;
+		m_cellPair.second->atomic_neighborCount.store(0);
+	}
 }
 
-Cell *World::GetCellAt(long a_cellX, long a_cellY)
+void World::ProcessPartContinuesly(unsigned int a_threadId, unsigned int a_threadCount)
 {
+	using std::pair;
+	unsigned long m_nextToGenerateGeneration = 1;
+	std::unique_lock<std::mutex> m_lk(this->currentGenerationLock);
+	auto m_iterator = this->cells.begin();
+	auto m_sectionEnd = this->cells.end();
+	auto m_lastEndPosition = this->cells.size();
+	long m_lastIteratorPos = 0;
+
+
+	while (!this->cancelSimulation)
+	{
+		// lk is only locked when the predicate is checked and if it returns true. 
+		// otherwise it will unlock lk allowing other threads to check the condition as well
+		// the whole loop set, see -> https://stackoverflow.com/a/2763749
+
+		this->nextUpdateCv.wait(m_lk, [this, m_nextToGenerateGeneration] {
+			return m_nextToGenerateGeneration <= this->currentGeneration || this->cancelSimulation;
+		});
+		// unlock lk. we only needed the lock to check the currentGeneration.
+		m_lk.unlock();
+		if (!this->cancelSimulation)
+		{
+			// Lock editing to the map
+			this->cellsEditLock.lock_shared();
+
+			long m_cellCount = this->cells.size();
+			long m_perThread = m_cellCount / a_threadCount;
+			long m_nextPosition = m_perThread * a_threadId;
+			// Get offset relative to the beginning
+			long m_difference = std::distance(this->cells.begin(), m_iterator);
+			// Calculate what number to move (negative or positive) to get to the m_nextPosition value
+			long m_toMove = m_nextPosition - m_difference;
+			std::advance(m_iterator, m_toMove);
+
+			// Update the ending of the end processor
+			if (m_nextPosition + m_perThread != m_lastEndPosition)
+			{
+				m_sectionEnd = this->cells.begin();
+				std::advance(m_sectionEnd, m_nextPosition + m_perThread);
+				m_lastEndPosition = m_nextPosition + m_perThread;
+			}
+
+			while (m_iterator != m_sectionEnd)
+			{
+				// An iterator item can be accessed like an pointer, you have to dereference it first.
+				Cell* m_cell = (*m_iterator).second;
+				if (m_cell->cellState == Tail)
+				{
+					m_cell->decayState = Conductor;
+				}
+				else if (m_cell->cellState == Head)
+				{
+					this->IncrementNeighbors(m_cell->x, m_cell->y);
+					m_cell->decayState = Tail;
+				}
+				std::advance(m_iterator, 1);
+			}
+			// Done with editing the map, unlock it again
+			this->cellsEditLock.unlock_shared();
+
+			// Notify main
+			auto m_threadCombo = this->threadComboData.find(a_threadId);
+			ThreadCombo* m_threadData = m_threadCombo->second;
+			m_threadData->lock.lock();
+			m_threadData->current_generation++;
+			m_threadData->lock.unlock();
+			m_threadData->cv.notify_all();
+			m_nextToGenerateGeneration++;
+		}
+		// lock the lk again.
+		m_lk.lock();
+	}
+}
+
+void World::ProcessLastPart()
+{
+	unsigned long m_nextToGenerateGeneration = 1;
+	std::unique_lock<std::mutex> m_lk(this->currentGenerationLock);
+	auto m_iterator = this->cells.begin();
+	auto m_sectionEnd = this->cells.end();
+	auto m_lastCellCount = this->cells.size();
+	long m_lastIteratorPos = 0;
+
+
+	while (!this->cancelSimulation)
+	{
+		// lk is only locked when the predicate is checked and if it returns true. 
+		// otherwise it will unlock lk allowing other threads to check the condition as well
+		// the whole loop set, see -> https://stackoverflow.com/a/2763749
+
+		this->nextUpdateCv.wait(m_lk, [this, m_nextToGenerateGeneration] {return m_nextToGenerateGeneration <= this->currentGeneration || this->cancelSimulation; });
+		// unlock lk. we only needed the lock to check the currentGeneration.
+		m_lk.unlock();
+		if (!this->cancelSimulation)
+		{
+			// Lock editing to the map
+			this->cellsEditLock.lock_shared();
+			long m_cellCount = this->cells.size();
+			long m_perThread = m_cellCount / this->totalThreads;
+			long m_nextPosition = m_perThread * (this->totalThreads - 1);
+			// Get offset relative to the beginning
+			long m_difference = std::distance(this->cells.begin(), m_iterator);
+			// Calculate what number to move (negative or positive) to get to the m_nextPosition value
+			long m_toMove = m_nextPosition - m_difference;
+			std::advance(m_iterator, m_toMove);
+
+			// Update the ending of the end processor
+			if (m_cellCount != m_lastCellCount)
+			{
+				m_sectionEnd = this->cells.end();
+				m_lastCellCount = m_nextPosition + m_perThread;
+			}
+
+			while (m_iterator != m_sectionEnd)
+			{
+				// An iterator item can be accessed like an pointer, you have to dereference it first.
+				Cell* m_cell = (*m_iterator).second;
+				if (m_cell->cellState == Tail)
+				{
+					m_cell->decayState = Conductor;
+				}
+				else if (m_cell->cellState == Head)
+				{
+					this->IncrementNeighbors(m_cell->x, m_cell->y);
+					m_cell->decayState = Tail;
+				}
+				std::advance(m_iterator, 1);
+			}
+
+			// Done with map, unlock it
+			this->cellsEditLock.unlock_shared();
+
+			// Notify main
+			this->lastPartLock.lock();
+			this->lastPartGeneration++;
+			this->lastPartLock.unlock();
+			this->lastPartGenerationCv.notify_all();
+			m_nextToGenerateGeneration++;
+		}
+		// lock the lk again.
+		m_lk.lock();
+	}
+}
+
+void World::IncrementNeighbors(long a_x, long a_y)
+{
+	std::pair<long, long> m_toFindPair;
+	auto m_end = this->cells.end();
+
+	// run through the Moore neighbors
+	for (char m_xOffset = -1; m_xOffset < 2; m_xOffset++)
+	{
+		for (char m_yOffset = -1; m_yOffset < 2; m_yOffset++)
+		{
+			if (!(m_yOffset == 0 && m_xOffset == 0))
+			{
+				m_toFindPair = std::make_pair(a_x + m_xOffset, a_y + m_yOffset);
+				auto m_found = this->cells.find(m_toFindPair);
+
+				// Only if we found the cell, and it has the state of Conductor. increment the neighbor count
+				if (m_found != m_end && m_found->second->cellState == Conductor)
+				{
+					m_found->second->atomic_neighborCount.fetch_add(1);
+				}
+			}
+		}
+	}
+}
+
+void World::SetTargetSpeed(float a_targetSpeed)
+{
+	{
+		std::lock_guard<std::mutex> m_lk(this->simCalcUpdateLock);
+		this->targetSimulationSpeed = a_targetSpeed;
+	}
+	this->simCalcUpdate.notify_all();
+}
+
+void World::TimerThread()
+{
+	bool m_stop = false;
+	while (!m_stop)
+	{
+		// Calculate the new time to wait for the next update
+		this->simCalcUpdateLock.lock();
+		float m_targetSpeed = this->targetSimulationSpeed;
+		long m_int = (long)(1000.0 / (double)m_targetSpeed);
+		auto m_nowTime = std::chrono::steady_clock::now();
+		bool m_pauzed = this->pauzeSimulation;
+		this->simCalcUpdateLock.unlock();
+
+		// Check if we should update the simulation (based on the paused state)
+		if (m_pauzed)
+		{
+			m_int = 1000; // just force it to a interval of 1s
+		}
+		else 
+		{
+			clock_t m_starTime = clock();
+			this->UpdateSimulationWithSingleGeneration();
+			clock_t m_endTime = clock();
+			float m_differenceInMs = (m_endTime - m_starTime) / (CLOCKS_PER_SEC / 1000.0f);
+			this->lastUpdateDuration = m_differenceInMs;
+		}
+
+		// Keep looping until we are suppose to wake up. 
+		// 1) That is either a the wait time has passed. 
+		// 2) A new target speed has been set and we should start updating at the new speed.
+		// 3) Or we are to cancel the simulation
+		auto m_nextWake = m_nowTime + std::chrono::milliseconds(m_int);
+		bool m_nextRun = false;
+		while (!m_nextRun)
+		{
+			std::unique_lock<std::mutex> m_lk(this->simCalcUpdateLock);
+			
+			// wait for either the simulation to be canceled or the speed to be changed
+			// returns false if timeout has expired and pred() is still false. otherwise it will return true
+			bool m_waitResult = this->simCalcUpdate.wait_until(m_lk, m_nextWake, [this, m_targetSpeed] {
+				return this->cancelSimulation || this->targetSimulationSpeed != m_targetSpeed;
+			});
+			if (m_waitResult)
+			{
+				if (this->cancelSimulation)
+				{
+					m_stop = true;
+					m_nextRun = true;
+				}
+				else
+				{
+					// Calculate the new time to wait, and set to wait if we need to, otherwise start the next update frame
+					m_int = (long)(1000.0 * this->targetSimulationSpeed);
+					auto m_newNextWake = m_nowTime + std::chrono::milliseconds(m_int);
+					auto m_now = std::chrono::steady_clock::now() - std::chrono::milliseconds(200);
+					if (m_newNextWake < m_now)
+					{
+						m_nextRun = true;
+					}
+					else
+					{
+						m_nextWake = m_newNextWake;
+					}
+				}
+			}
+			else
+			{
+				m_nextRun = true;
+			}
+			m_lk.unlock();
+		}
+	}
+}
+
+Cell* World::GetCopyOfCellAt(long a_cellX, long a_cellY)
+{
+	std::lock_guard<std::shared_mutex> m_lk(this->cellsEditLock);
 	// Retrieves the pointer of a cell at a specific grid coordinate
-	return nullptr;
+	auto m_found = this->cells.find(std::make_pair(a_cellX, a_cellY));
+	if (m_found == this->cells.end())
+	{
+		return nullptr;
+	}
+	else
+	{
+		return new Cell(m_found->second->x, m_found->second->y, m_found->second->cellState);
+	}
+}
+
+bool World::InsertCellAt(long a_cellX, long a_cellY, CellState a_state)
+{
+	std::lock_guard<std::shared_mutex> m_lk(this->cellsEditLock);
+	if (this->cells.find(std::make_pair(a_cellX, a_cellY)) == this->cells.end())
+		return false;
+	this->cells.insert(std::make_pair(std::make_pair(a_cellX, a_cellY), new Cell(a_cellX, a_cellY, a_state)));
+	return true;
+}
+
+bool World::UpdateCellState(long a_cellX, long a_cellY, std::function<bool (Cell*)> a_updater)
+{
+	std::lock_guard<std::shared_mutex> m_lk(this->cellsEditLock);
+	auto m_found = this->cells.find(std::make_pair(a_cellX, a_cellY));
+	if (m_found == this->cells.end())
+	{
+		return false;
+	}
+	else
+	{
+		return a_updater(m_found->second);
+	}
 }
 
 void World::LoadBlockAt(PremadeBlock a_premadeBlock, long a_cellX, long a_cellY)
--- src/world.h
+++ src/world.h
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <map>
+#include <functional> // create your own lambda
+#include <vector>
+#include <condition_variable>
+#include <shared_mutex>
 
 #include "cell.h"
 #include "premadeBlock.h"
@@ -10,17 +15,50 @@
 
 class World
 {
+
 private:
+	class ThreadCombo {
+		public:
+			std::mutex lock;
+			std::condition_variable cv;
+			unsigned long current_generation;
+			ThreadCombo()
+			{
+				this->current_generation = 0;
+			};
+	};
+
 	std::string filePath;
+
+	std::thread timerThread;
+	std::mutex simCalcUpdateLock;
+	std::condition_variable simCalcUpdate;
 	bool cancelSimulation = false;
+	bool pauzeSimulation = false;
 
+	std::mutex currentGenerationLock;
+
+	std::vector<std::thread> simUpdaters;
+	std::condition_variable nextUpdateCv;
+	std::map<int, ThreadCombo*> threadComboData;
+	unsigned int totalThreads;
+
+	std::mutex lastPartLock;
+	std::condition_variable lastPartGenerationCv;
+	unsigned long lastPartGeneration;
+	// The thread that processes the last parts of the cells list
+	std::thread lastPartProcessor;
+	// Simulation speed in Hz
+	float targetSimulationSpeed = 0.0f;
+
+	// Lock for when you need to edit the cells
+	std::shared_mutex cellsEditLock;
 public:
-	Cell cells[1];
+	std::map<std::pair<long, long>, Cell*> cells;
 
-	int lastRenderDuration = 0;
 	unsigned long currentGeneration = 0;
+	float lastUpdateDuration = 0;
 
-	float targetSimulationSpeed = 1.0f;
 
 	std::string description;
 	std::string author;
@@ -28,18 +66,35 @@ public:
 private:
 	void LoadFile();
 	void EmptyWorld();
-
+	void IncrementNeighbors(long a_x, long a_y);
+	void ProcessPartContinuesly(unsigned int  a_threadId, unsigned int a_maxThreads);
+	void ProcessLastPart();
+	void TimerThread();
+	void World::InitializeThreads();
 public:
+	World();
+	// Copy constructor
+	World(const World& a_that);
+	// Copy assignment operator
+	World& operator=(const World& a_that);
+	
+	~World();
+
 	void Save(std::string a_worldName);
 	void Open(std::string a_filePath);
 	void SaveAsTemplate(std::string a_worldName);
 
+	void UpdateSimulationWithSingleGeneration();
 	void StartSimulation();
 	void PauzeSimulation();
 	void ResetSimulation();
-	void UpdateSimulation();
 
-	Cell *GetCellAt(long a_cellX, long a_cellY);
+	void SetTargetSpeed(float a_targetSpeed);
+	float GetTargetSpeed() { return this->targetSimulationSpeed; };
+
+	Cell* GetCopyOfCellAt(long a_cellX, long a_cellY);
+	bool UpdateCellState(long a_cellX, long a_cellY, std::function<bool (Cell*)> a_updater);
+	bool InsertCellAt(long a_cellX, long a_cellY, CellState a_state);
 	void LoadBlockAt(PremadeBlock a_premadeBlock, long a_cellX, long a_cellY);
 };
 
